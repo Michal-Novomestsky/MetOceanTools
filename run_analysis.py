c59e6d2f57b6681da9def236240c94040bbc09c8
Michal Novomestsky <mnov0010@student.monash.edu>
2023-07-21 13:54:09 +1000

    refactor: spaces to comments

--- run_analysis.py
+++ run_analysis.py
@@ -409,9 +409,9 @@ def preprocess(eraDf: pd.DataFrame, remsDf: pd.DataFrame, writeDir: os.PathLike)
     time_j = []
     solrad_j = []
     time_delta = remsDf.timemet[len(remsDf) - 1] - remsDf.timemet[0]
-    amountOfSlices = time_delta.total_seconds()//3600 #seconds -> hours
+    amountOfSlices = time_delta.total_seconds()//3600 # seconds -> hours
     solSlices = np.array_split(remsDf, amountOfSlices)
-    #Integrating over each hour
+    # Integrating over each hour
     for slice in solSlices:
         slice = slice.reset_index()
         xVals = slice.timemet - slice.timemet[0]
@@ -422,9 +422,9 @@ def preprocess(eraDf: pd.DataFrame, remsDf: pd.DataFrame, writeDir: os.PathLike)
     time_j = []
     thermrad_j = []
     time_delta = remsDf.timemet[len(remsDf) - 1] - remsDf.timemet[0]
-    amountOfSlices = time_delta.total_seconds()//3600 #seconds -> hours
+    amountOfSlices = time_delta.total_seconds()//3600 # seconds -> hours
     solSlices = np.array_split(remsDf, amountOfSlices)
-    #Integrating over each hour
+    # Integrating over each hour
     for slice in solSlices:
         slice = slice.reset_index()
         xVals = slice.timemet - slice.timemet[0]
@@ -433,8 +433,8 @@ def preprocess(eraDf: pd.DataFrame, remsDf: pd.DataFrame, writeDir: os.PathLike)
         time_j.append(slice.timemet[len(slice) - 1])
 
     # TODO: PATCH FIX
-    # eraDf.solrad = eraDf.solrad/3600
-    # eraDf.thermrad = eraDf.thermrad/3600
+    eraDf.solrad = eraDf.solrad/3600
+    eraDf.thermrad = eraDf.thermrad/3600
 
     sns.lineplot(x=time_j, y=solrad_j, markers=True, label='REMS')
     sns.lineplot(data=eraDf, x='timemet', y='solrad', markers=True, label='ERA5')
