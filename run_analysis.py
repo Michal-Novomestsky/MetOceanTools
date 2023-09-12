bad87aa96b59d2d70be659fe7bb10568b1d5d455
Michal Novomestsky <mnov0010@student.monash.edu>
2023-09-13 00:13:03 +1000

    rollback to old cov

--- run_analysis.py
+++ run_analysis.py
@@ -399,8 +399,9 @@ def get_covariance(u: np.ndarray, v: np.ndarray) -> float:
     :param v: (np.ndarray) Var 2.
     :return: (float) cov(u, v)
     '''
-    logical = (pd.notna(u)) & (pd.notna(v))
-    return np.cov(u[logical], v[logical])[0][1]
+    # logical = (pd.notna(u)) & (pd.notna(v))
+    # return np.cov(u[logical], v[logical])[0][1]
+    return np.cov(u, v)[0][1]
 
 def preprocess(eraDf: pd.DataFrame, remsDf: pd.DataFrame, writeDir: os.PathLike, era_only: bool, save_plots=True, time_lim=None) -> pd.DataFrame:
     '''
