46a0b2b7351b4f9587468c86514c40d44be2169c
Michal Novomestsky <mnov0010@student.monash.edu>
2023-09-25 13:42:16 +1000

    flipped u2 and v2 inside mru_correct

--- Modules/DataCleaner.py
+++ Modules/DataCleaner.py
@@ -559,9 +559,10 @@ class DataCleaner:
                     + w1_temp*np.cos(np.deg2rad(self.df[mru_pitch]))*np.cos(np.deg2rad(self.df[mru_roll]))
 
         # Sign flipping to correct for bug
-        # TODO NOT DONE FOR ANEM 2
         self.df[u1] = -self.df[u1]
         self.df[v1] = -self.df[v1]
+        self.df[u2] = -self.df[u2]
+        self.df[v2] = -self.df[v2]
 
     def remove_nans(self, entry: str, df: pd.DataFrame, naive=False) -> None:
         """
