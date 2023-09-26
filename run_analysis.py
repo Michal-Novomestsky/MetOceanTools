21f026c0bb6a29283e92846963db5524b836f4f4
Michal Novomestsky <mnov0010@student.monash.edu>
2023-09-26 17:42:25 +1000

    test: only correcting u10

--- run_analysis.py
+++ run_analysis.py
@@ -368,8 +368,8 @@ def get_windspeed_data(slice: pd.Series, u: str, v: str, w: str, t: str) -> tupl
     U_mag_corr = w_vel*np.sin(theta) + U_mag*np.cos(theta)
     w_vel_corr = w_vel*np.cos(theta) - U_mag*np.sin(theta)
 
-    # U_mag = U_mag_corr
-    w_vel = w_vel_corr
+    U_mag = U_mag_corr
+    # w_vel = w_vel_corr
 
     # Getting magnitude of turbulent horizontal velocity vector
     U_turb = get_turbulent(U_mag)
